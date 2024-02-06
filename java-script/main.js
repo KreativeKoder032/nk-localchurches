c4455b7ac85ef8c68eb832e29a40470c9d49d08a
Koehler, Niklas <nkoehler001@student.franciscan.edu>
2024-02-06 12:14:00 -0500

    .

---
+++ java-script/main.js
@@ -0,0 +1,7 @@
+/**
+ * this is my JavaScript file.
+ */
+let div = document.getElementById("myDiv")
+console.log(div) //sends this to the console
+div.innerHTML = "This is from my JavaScript";
+window.alert("This is from my JavaScript.")
